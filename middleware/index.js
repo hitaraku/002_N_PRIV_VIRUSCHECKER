1c244dfb89db628ac3468a1b3e82f64ff46067d8
penny12 <airu85@live.jp>
2020-03-03 12:37:12 +0000

    implement dictionary store mongodatabase

---
+++ middleware/index.js
@@ -0,0 +1,148 @@
+var Coronavirustimeline = require("../models/coronavirustimeline"),
+    Countrydictionary = require("../models/countrydictionary");
+
+/**
+ * This Section is translate and mongoose store function *
+**/
+const projectId = 'n-priv-viruschecker';
+const location = 'global';
+const text = '英国（含北爱尔兰）';
+
+// Imports the Google Cloud Translation library
+const {TranslationServiceClient} = require('@google-cloud/translate');
+
+// Instantiates a client
+const translationClient = new TranslationServiceClient();
+async function translateText(sourceString, targetString, textString) {
+  // Construct request
+  const request = {
+    parent: `projects/${projectId}/locations/${location}`,
+    contents: [textString],
+    mimeType: 'text/plain', // mime types: text/plain, text/html
+    sourceLanguageCode: sourceString,
+    targetLanguageCode: targetString,
+  };
+
+  try {
+    // Run request
+    const [response] = await translationClient.translateText(request);
+
+    for (const translation of response.translations) {
+      console.log(`Translation: ${translation.translatedText}`);
+                                
+      if(targetString == "ja-JP") {
+        // For date
+        var dateTime = new Date().toLocaleString("en-US", {timeZone: "Asia/Tokyo"});
+                            
+        var newCountrydictionary = {
+            chinese: textString,
+            japanese: translation.translatedText,
+            // english: String,
+            // spanish: String,
+            gotDate: dateTime,
+        }          
+
+        // create a new Countrydictionary to DB.
+        Countrydictionary.create(newCountrydictionary, function(err, newlyCreated){
+            if(err) {
+                console.log(err);
+            } else {
+                console.log("success store cornavirus to database");
+            }
+        });
+      } else if(targetString == "en") {
+          const filter = { chinese: textString };
+          const update = { english: translation.translatedText };
+          console.log("textString : " + textString);
+          console.log("english translation.translatedText : " + translation.translatedText);
+          Countrydictionary.findOneAndUpdate(filter, update).exec(function(err, a){
+                if(err) {
+                    console.log(err);
+                } else {
+                    // none
+                }
+              });
+      } else if(targetString == "es") {
+          const filter = { chinese: textString };
+          const update = { spanish: translation.translatedText };
+          console.log("textString : " + textString);
+          console.log("spanish translation.translatedText : " + translation.translatedText);
+          Countrydictionary.findOneAndUpdate(filter, update).exec(function(err, a){
+                if(err) {
+                    console.log(err);
+                } else {
+                    // none
+                }
+              });
+      }
+    }
+  } catch (error) {
+    console.error(error.details);
+  }
+}
+
+/**
+ * Middle ware for dictionary *
+**/
+module.exports.namedic = function(req, res, next) {
+        // For get all coronaCountryname
+    Coronavirustimeline.findOne({}).sort({ gotDate: 'desc'}).exec(function(err, latestCoronavirustimeline){
+        if(err) {
+            console.log(err);
+        } else {
+            /*
+            * This Result is from china goverment data
+            */
+            var coronavirustimelines = latestCoronavirustimeline.cornavirusoverall.results;
+            var gotDate = latestCoronavirustimeline.gotDate;
+            
+            var arrayCorona = [];
+            coronavirustimelines.forEach(function(coronavirustimeline, i){
+                arrayCorona.push(coronavirustimeline.countryName);
+            });
+            // 重複を削除したリスト
+            var repeatDeleteArray = arrayCorona.filter(function (x, i, self) {
+                return self.indexOf(x) === i;
+            });
+            
+            /*
+            * This Result is from My Mongo database.
+            */
+            Countrydictionary.find({}).exec(function(err, latestCountrydictionarys) {
+                if(err) {
+                    console.log(err);
+                } else {
+                    var mongoDicArray = [];
+                    for(let i = 0; i < latestCountrydictionarys.length; i++) {
+                        mongoDicArray.push(latestCountrydictionarys[i].chinese);
+                    }
+                    console.log(mongoDicArray);
+                    
+                    // Find alrady stored dictionary
+                    for(let i = 0; i < repeatDeleteArray.length; i++) {
+                        const found = mongoDicArray.find(element => element == repeatDeleteArray[i]);
+                        if(found != undefined) {
+                            // none
+                            // console.log("found : " + found);
+                        } else {
+                            console.log("not found : " + repeatDeleteArray[i]);
+                            
+                            /*
+                            * not find string store to mongo database
+                            */
+                         
+                            // For Create JP
+                            translateText('zh', 'ja-JP', repeatDeleteArray[i]);
+
+                            // For English
+                            translateText('zh', 'en', repeatDeleteArray[i]);
+            
+                            // For Spanish
+                            translateText('zh', 'es', repeatDeleteArray[i]);
+                        }
+                    }
+                }
+            });
+        }
+    });
+}
---
+++ models/countrydictionary.js
@@ -0,0 +1,12 @@
+var mongoose = require("mongoose");
+
+var CountrydictionarySchema = new mongoose.Schema({
+    chinese: String,
+    japanese: String,
+    english: String,
+    spanish: String,
+    
+    gotDate: Date,
+});
+
+module.exports = mongoose.model("Countrydictionary", CountrydictionarySchema);
